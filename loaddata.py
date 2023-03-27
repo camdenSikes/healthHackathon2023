637fba19a1222f8801e6415322b615dcd159e3b4
camdenSikes <camdensikes@gmail.com>
2023-03-27 09:09:12 -0500

    Finished analysis for initial presentation

--- loaddata.py
+++ loaddata.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import math
 from matplotlib import pyplot as plt
+import numpy as np
+from scipy import stats
+
+INFRATE = 1.06
 
 class DataHolder:
     #In provided dataset, qualmeas.xlsx
@@ -14,10 +18,24 @@ class DataHolder:
     adasthma = None
     adasthmaDenom = None
 
+    #In provided datset, cost.xlsx
+    #Children TCOC
+    ctcoc = None
+    costMedGrp = None
+    #Inpatient Admission Ratio
+    inRat = None
+    #ER Visits Ratio
+    erRat = None
+    #Primary Care Visits Ratio
+    pcRat = None
+
     #In incomedata.csv
     totHouse = 0
     medIncome = 0
     meanIncome = 0
+
+    #In ruralzip.xlsx
+    isRural = False
 
     def __init__(self) -> None:
         self.adscreen = 0
@@ -29,6 +47,12 @@ class DataHolder:
         self.totHouse = 0
         self.medIncome = 0
         self.meanIncome = 0
+        self.ctcoc = []
+        self.costMedGrp = []
+        self.inRat = []
+        self.erRat = []
+        self.pcRat = []
+        self.isRural = False
 
     def updateAdscreen(self, rate, denom):
         newDenom = self.adscreenDenom + denom
@@ -44,7 +68,6 @@ class DataHolder:
         newDenom = self.adasthmaDenom + denom
         self.adasthma = (self.adasthma*self.adasthmaDenom + rate*denom)/newDenom
         self.adasthmaDenom = newDenom
-
 
     def setIncome(self,totHouse,medIncome,meanIncome):
         self.totHouse = totHouse
@@ -116,6 +139,31 @@ for row in df.index:
                     zipdict2018[zip] = DataHolder()
                 zipdict2018[zip].updateAsthma(rate,denom)
 
+
+df = pd.read_excel(r"C:\\Users\\gcdas\\Git\\healthHackathon2023\\cost.xlsx",0)
+for row in df.index:
+    year = df.loc[row,'Measurement Year']
+    ctcoc = df.loc[row,'Children TCOC']
+    medGrp = df.loc[row, 'Medical Group Name']
+    inRat = df.loc[row, 'Inpatient Admission Ratio']
+    erRat = df.loc[row, 'ER Visits Ratio']
+    pcRat = df.loc[row, 'Primary Care Visits Ratio']
+    if math.isnan(ctcoc):
+        continue
+    if year == 2021:
+        totaldh2021.ctcoc.append(ctcoc)
+        totaldh2021.costMedGrp.append(medGrp)
+        totaldh2021.inRat.append(inRat)
+        totaldh2021.erRat.append(erRat)
+        totaldh2021.pcRat.append(pcRat)
+    else:
+        totaldh2018.ctcoc.append(ctcoc)
+        totaldh2018.costMedGrp.append(medGrp)
+        totaldh2018.inRat.append(inRat)
+        totaldh2018.erRat.append(erRat)
+        totaldh2018.pcRat.append(pcRat)
+
+
 incomedf = pd.read_csv(r"incomedata.csv")
 sumtotHouse2018 = 0
 summedIncome2018 = 0
@@ -141,6 +189,14 @@ for row in incomedf.index:
 totaldh2018.setIncome(sumtotHouse2018,summedIncome2018/sumtotHouse2018,summeanIncome2018/sumtotHouse2018)
 totaldh2021.setIncome(sumtotHouse2021,summedIncome2021/sumtotHouse2021,summeanIncome2021/sumtotHouse2021)
 
+ruraldf = pd.read_excel(r"ruralzip.xlsx",1)
+for row in ruraldf.index:
+    zip = ruraldf.loc[row,'ZIP_CODE']
+    ruca = ruraldf.loc[row,'RUCA1']
+    if zip in zipdict2018:
+        zipdict2018[zip].isRural = (ruca>3)
+
+
 ## plot med income vs screen for both years
 # medIncome2018 = []
 # adscreen2018 = []
@@ -158,27 +214,102 @@ totaldh2021.setIncome(sumtotHouse2021,summedIncome2021/sumtotHouse2021,summeanIn
 #         adscreen2021.append(dh.adscreen)
 # plt.plot(medIncome2021,adscreen2021,'bo')
 
-#Plot med income vs. change in phq util
-medIncome = []
-deltascreen = []
+# #Plot med income vs. change in phq util
+# medIncome = []
+# deltascreen = []
+# for zip,dh in zipdict2018.items():
+#      if(dh.medIncome != 0) and zip in zipdict2021 and dh.adPHQDenom != 0 and zipdict2021[zip].adPHQDenom != 0:
+#          medIncome.append(dh.medIncome)
+#          deltascreen.append(zipdict2021[zip].adPHQ - dh.adPHQ)
+# plt.plot(medIncome,deltascreen,'ro')
+# plt.show()
+
+
+# #Look at changes in various cost measures
+# deltatcoc = []
+# deltain = []
+# deltaer = []
+# deltapc = []
+# for i,place in enumerate(totaldh2018.costMedGrp):
+#     if place not in totaldh2021.costMedGrp:
+#         continue
+#     ind = totaldh2021.costMedGrp.index(place)
+#     deltatcoc.append(totaldh2021.ctcoc[ind] - totaldh2018.ctcoc[i]*INFRATE)
+#     deltain.append(totaldh2021.inRat[ind] - totaldh2018.inRat[i])
+#     deltaer.append(totaldh2021.erRat[ind] - totaldh2018.erRat[i])
+#     deltapc.append(totaldh2021.pcRat[ind] - totaldh2018.pcRat[i])
+#
+# plt.violinplot(deltatcoc)
+# plt.axhline()
+# plt.tick_params('x',which='both',top=False,bottom=False,labelbottom=False)
+# plt.title("Change in Children's Total Cost of Care")
+# plt.ylabel("Change in Cost (dollars)")
+# plt.show()
+# plt.violinplot(deltapc)
+# plt.axhline()
+# plt.tick_params('x',which='both',top=False,bottom=False,labelbottom=False)
+# plt.title("Change in Primary Care Visits Ratio")
+# plt.ylabel("Change in Ratio")
+# plt.show()
+# print(stats.ttest_1samp(deltatcoc,0))
+# print(stats.ttest_1samp(deltain,0))
+# print(stats.ttest_1samp(deltaer,0))
+# print(stats.ttest_1samp(deltapc,0))
+
+# #Plot med income vs. change in asthma
+# medIncome = []
+# meanIncome = []
+# deltaasthma = []
+# asthma = []
+# for zip,dh in zipdict2018.items():
+#      if(dh.medIncome != 0) and zip in zipdict2021 and dh.adasthmaDenom != 0 and zipdict2021[zip].adasthmaDenom != 0:
+#          medIncome.append(dh.medIncome)
+#          meanIncome.append(dh.meanIncome)
+#          deltaasthma.append(zipdict2021[zip].adasthma - dh.adasthma)
+#          asthma.append(dh.adasthma)
+# plt.violinplot(deltaasthma)
+# plt.axhline()
+# plt.tick_params('x',which='both',top=False,bottom=False,labelbottom=False)
+# plt.title("Change in Optimal Asthma Control in Children")
+# plt.ylabel("Change in Proportion")
+# plt.show()
+# print(stats.ttest_1samp(deltaasthma,0))
+# y = np.array(asthma)
+# x = np.array(medIncome)
+# A = np.vstack([x, np.ones(len(x))]).T
+# alpha = np.linalg.lstsq(A,y)
+# slope, intercept, r_value, p_value, std_err = stats.linregress(x, y)
+# plt.plot(medIncome,asthma,'b.')
+# plt.plot(x,slope*x + intercept,'r')
+# plt.title("Optimal Asthma Control in Children vs. Median Income by ZIP Code")
+# plt.xlabel("Median Household Income (dollars)")
+# plt.ylabel("Proportion of Children with Optimal Asthma Control")
+# plt.show()
+# print("r^2 =" + str(r_value**2))
+
+#Plot asthma rural vs urban
+deltaasthmaUrb = []
+asthmaUrb = []
+deltaasthmaRur = []
+asthmaRur = []
 for zip,dh in zipdict2018.items():
-     if(dh.medIncome != 0) and zip in zipdict2021 and dh.adPHQDenom != 0 and zipdict2021[zip].adPHQDenom != 0:
-         medIncome.append(dh.medIncome)
-         deltascreen.append(zipdict2021[zip].adPHQ - dh.adPHQ)
-plt.plot(medIncome,deltascreen,'ro')
-
+    if zip in zipdict2021 and dh.adasthmaDenom != 0 and zipdict2021[zip].adasthmaDenom != 0:
+        if(zipdict2018[zip].isRural):
+            deltaasthmaRur.append(zipdict2021[zip].adasthma - dh.adasthma)
+            asthmaRur.append(dh.adasthma)
+        else:
+            deltaasthmaUrb.append(zipdict2021[zip].adasthma - dh.adasthma)
+            asthmaUrb.append(dh.adasthma)
+print(stats.ttest_ind(deltaasthmaRur,deltaasthmaUrb))
+plt.violinplot([asthmaUrb,asthmaRur],showmedians=True)
+plt.xticks([1,2],["Urban","Rural"])
+plt.title("Distributions of Children's Optimal Asthma Control at Urban and Rural Clinics")
+plt.ylabel("Proportion of Children with Optmal Asthma Control")
 plt.show()
+print(stats.ttest_ind(asthmaRur,asthmaUrb))
 
-#Plot med income vs. change in asthma
-medIncome = []
-deltascreen = []
-for zip,dh in zipdict2018.items():
-     if(dh.medIncome != 0) and zip in zipdict2021 and dh.adasthmaDenom != 0 and zipdict2021[zip].adasthmaDenom != 0:
-         medIncome.append(dh.medIncome)
-         deltascreen.append(zipdict2021[zip].adasthma - dh.adasthma)
-plt.plot(medIncome,deltascreen,'ro')
 
-plt.show()
+
 
 
 
